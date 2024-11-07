6be192c04087d1b7936e7e25f8dcf99911b68c5a
Rafaartdev <silveira.orafa@gmail.com>
2024-11-07 15:02:48 -0300

    mascara Cep/fone

--- js/contato.js
+++ js/contato.js
@@ -9,6 +9,10 @@ const campoEstado = document.querySelector("#estado");
 const botaoBuscar = document.querySelector("#buscar");
 const mensagemStatus = document.querySelector("#status");
 
+//Ativaçãao das mascaras para telefone e CEP
+$(campoTelefone).mask("(00) 0000-0000");
+$(campoCep).mask("00000-000");
+
 //Ouvinte de evento para o botão Buscar 
 botaoBuscar.addEventListener("click", async function () {
     //Verififando se o CEP digitado não tem 9 digitos
